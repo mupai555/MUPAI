9f2328a6e2e3713c1cf111ecca8e12e8f1ff7776
mupai555 <erickdeluna55@hotmail.com>
2024-12-23 00:45:27 -0600

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -105,6 +105,7 @@ def cuestionario_calidad_sueno():
         else:
             st.error("Mala calidad de sueño. Considera consultar a un especialista.")
 
+# Definir la función para Nivel de Actividad Física 
 def cuestionario_ipaq():
     st.title("Cuestionario de Actividad Física - IPAQ (Versión Corta)")
     st.write("Responde las siguientes preguntas sobre tu actividad física durante los últimos 7 días.")
@@ -360,7 +361,7 @@ elif menu == "Evaluación del Estilo de Vida":
         cuestionario_calidad_sueno()  # Llama la función de Calidad del Sueño
    
     elif submenu == "Nivel de Actividad Física":
-    cuestionario_ipaq():  # Llama al cuestionario IPAQ
+    cuestionario_ipaq():  # Llama la función para Nivel de Actividad Física
 
 
 # Función para el cuestionario de Calidad del Sueño
