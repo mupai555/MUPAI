ed7b73df306a6a824302344ad793bb14f619fc1b
mupai555 <erickdeluna55@hotmail.com>
2024-12-27 01:27:27 -0600

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -564,7 +564,7 @@ elif menu == "Evaluación del Estilo de Vida":
       cuestionario_habitos_alimenticios()  # Llama la función para Hábitos Alimenticios 
 
     elif submenu == "Potencial Genético Muscular":         
-           evaluacion_potencial_genetico() # Llama la función para Potencial Genético Muscular
+       evaluacion_potencial_genetico() # Llama la función para Potencial Genético Muscular
         
 
 
