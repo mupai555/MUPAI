848993a8a8e040d6256bb46454c5d411db1269f0
mupai555 <erickdeluna55@hotmail.com>
2025-07-15 19:58:17 -0600

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -4,6 +4,7 @@ import numpy as np
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.header import Header
 from datetime import datetime
 import base64
 from collections import Counter
